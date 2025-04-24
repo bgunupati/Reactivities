87b05c888228a8f0a0c2550cb8d84bc7f4b50231
bgunupati <bgunupati@gmail.com>
2025-04-24 17:37:11 +0530

    Added Select and View in UI

---
+++ client/src/features/activities/dashboard/ActivityDashboard.tsx
@@ -0,0 +1,32 @@
+import { Grid } from "@mui/material";
+import ActivityList from "./ActivityList";
+import ActivityDetails from "../details/activitydetails";
+
+type Props = {
+    activities: Activity[]
+    selectActivity: (id: string) => void;
+    cancelSelectActivity: () => void;
+    selectedActivity?: Activity;
+}
+
+export default function ActivityDashboard({ activities, cancelSelectActivity,
+    selectActivity, selectedActivity }: Props) {
+    return (
+        <Grid container spacing={3}>
+            <Grid size={7}>
+                <ActivityList
+                    activities={activities}
+                    selectActivity={selectActivity}
+                />
+            </Grid>
+            <Grid size={5}>
+                {selectedActivity && 
+                <ActivityDetails
+                    activity={selectedActivity}
+                    cancelSelectActivity={cancelSelectActivity}
+                />
+                }
+            </Grid>
+        </Grid>
+    )
+}
